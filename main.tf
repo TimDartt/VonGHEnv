655765ce8cea731f962ec478c5ec28419020dab0
TimDartt <tdartt@gmail.com>
2023-03-02 14:01:10 -0500

    Test run

--- main.tf
+++ main.tf
@@ -1,32 +1,26 @@
 # Create The Networking resource group
 
-
-# first create the resource group --
-# resource "azurerm_resource_group" "gh-networking" {
-
-# }
-
-
-# module "GlobalGHNet" {
-#   source         = "./modules/Network"
-#   subNet         = "1"                                      # not currently in use
-#   security_rules = concat(var.SQLSecRules, var.IISSecRules) #these are loose at best. We still need to setup ASG's
-#   secGroupName   = "GlobalHealthNSGSecurity"                #this will eventually need to be modified to reflect the subnet/environment
-#   resourceGroup  = azurerm_resource_group.vonGlobalHealhRG.name
-#   networkName    = "GlobalHealthNet"
-# }
-
-
-
 # Define and build the Resource Groups
 # by prebuilding the resources and groups we can reference them later in the process without having to build them as an initial step
-
 module "resourceGroup" {
   source            = "./modules/ResourceGroups"
   for_each          = { for index, item in var.ResourceGroups : index => item }
   ResourceGroupName = each.value.resource_group_name
   ResourceLocation  = each.value.location
 }
+
+
+#define the network resource... 
+module "Network_Building" {
+  source         = "./modules/Network"
+  subNet         = "1"                                      # not currently in use
+  security_rules = concat(var.SQLSecRules, var.IISSecRules) #these are loose at best. We still need to setup ASG's
+  secGroupName   = "GlobalHealthNSGSecurity"                #this will eventually need to be modified to reflect the subnet/environment
+  resourceGroup  = azurerm_resource_group.vonGlobalHealhRG.name
+  networkName    = "GlobalHealthNet"
+}
+
+
 
 
 # resource "azurerm_resource_group" "vonGlobalHealhRG" {
