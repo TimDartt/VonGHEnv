edf8322eb9096f55dac94b530d5a39e07fe1fd1a
TimDartt <tdartt@gmail.com>
2023-03-06 09:29:57 -0500

    Adding all the subnets

--- modules/Network/vNets/main.tf
+++ modules/Network/vNets/main.tf
@@ -34,74 +34,74 @@ resource "azurerm_virtual_network" "test" {
     name           = "AzureFirewallSubnet"
     address_prefix = "10.140.100.0/24"
   }
-  # subnet {
-  #   name           = "AzureFirewallManagementSubnet"
-  #   address_prefix = "10.150.90.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-private-1"
-  #   address_prefix = "10.150.70.0/24"
-  # }
-  # subnet {
-  #   name           = "GatewaySubnet"
-  #   address_prefix = "10.150.0.0/24"
-  # }
-  # subnet {
-  #   name           = "GH-Scaffold-VM"
-  #   address_prefix = "10.150.95.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaffold-lb-priv-backendservers"
-  #   address_prefix = "10.150.10.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaffold-lb-priv-standard-internal"
-  #   address_prefix = "10.150.11.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaffold-lb-priv-private-linkservice"
-  #   address_prefix = "10.150.12.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaff-asg-frontend"
-  #   address_prefix = "10.150.20.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaff-asg-controlplane"
-  #   address_prefix = "10.150.21.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaff-asg-backend"
-  #   address_prefix = "10.150.22.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaff-asg-connector"
-  #   address_prefix = "10.150.23.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-public-1"
-  #   address_prefix = "10.150.60.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaffold-private-dns-subnet"
-  #   address_prefix = "10.150.96.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaffold-dns-internal"
-  #   address_prefix = "10.150.45.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-scaffold-dns-outbound"
-  #   address_prefix = "10.150.46.0/24"
-  # }
-  # subnet {
-  #   name           = "gh-internal-1"
-  #   address_prefix = "10.150.80.0/24"
-  # }
+  subnet {
+    name           = "AzureFirewallManagementSubnet"
+    address_prefix = "10.140.90.0/24"
+  }
+  subnet {
+    name           = "gh-private-1"
+    address_prefix = "10.140.70.0/24"
+  }
+  subnet {
+    name           = "GatewaySubnet"
+    address_prefix = "10.140.0.0/24"
+  }
+  subnet {
+    name           = "GH-Scaffold-VM"
+    address_prefix = "10.140.95.0/24"
+  }
+  subnet {
+    name           = "gh-scaffold-lb-priv-backendservers"
+    address_prefix = "10.140.10.0/24"
+  }
+  subnet {
+    name           = "gh-scaffold-lb-priv-standard-internal"
+    address_prefix = "10.140.11.0/24"
+  }
+  subnet {
+    name           = "gh-scaffold-lb-priv-private-linkservice"
+    address_prefix = "10.140.12.0/24"
+  }
+  subnet {
+    name           = "gh-scaff-asg-frontend"
+    address_prefix = "10.140.20.0/24"
+  }
+  subnet {
+    name           = "gh-scaff-asg-controlplane"
+    address_prefix = "10.140.21.0/24"
+  }
+  subnet {
+    name           = "gh-scaff-asg-backend"
+    address_prefix = "10.140.22.0/24"
+  }
+  subnet {
+    name           = "gh-scaff-asg-connector"
+    address_prefix = "10.140.23.0/24"
+  }
+  subnet {
+    name           = "gh-public-1"
+    address_prefix = "10.140.60.0/24"
+  }
+  subnet {
+    name           = "gh-scaffold-private-dns-subnet"
+    address_prefix = "10.140.96.0/24"
+  }
+  subnet {
+    name           = "gh-scaffold-dns-internal"
+    address_prefix = "10.140.45.0/24"
+  }
+  subnet {
+    name           = "gh-scaffold-dns-outbound"
+    address_prefix = "10.140.46.0/24"
+  }
+  subnet {
+    name           = "gh-internal-1"
+    address_prefix = "10.140.80.0/24"
+  }
 
   # subnet {
   #   name           = "core-routing"
-  #   address_prefix = "10.150.50.0/24"
+  #   address_prefix = "10.140.50.0/24"
   #   security_group = azurerm_network_security_group.envSecGroup.id
   # }
   #set the tags...
