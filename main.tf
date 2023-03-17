fa1e54e8ad25e20108e6c1cabb04d17170ee92c1
TimDartt <tdartt@gmail.com>
2023-03-17 07:49:32 -0400

    Test creating JUST the resource groups

--- main.tf
+++ main.tf
@@ -11,65 +11,65 @@ module "resourceGroup" {
   tags              = each.value.tags
 }
 
-# #define the first network resource... 
-# #first the resource group to contain the networking elements
-# ** Testing Depends_on
-resource "azurerm_resource_group" "gh-networking" {
-  name     = "gh-networking"
-  location = local.location
-  tags     = local.tags
-}
-# build any needed vNets
-module "vNets" {
-  source        = "./modules/Network/vNets"
-  networkName   = "scaffold-core"
-  location      = "eastus"
-  tags          = local.tags
-  BaseNet       = var.BaseNet
-  SubNets       = local.Scaffold-Core-SubNets # the list of Subnets to create for the vNet
-  resourceGroup = "gh-networking"
+# # #define the first network resource... 
+# # #first the resource group to contain the networking elements
+# # ** Testing Depends_on
+# resource "azurerm_resource_group" "gh-networking" {
+#   name     = "gh-networking"
+#   location = local.location
+#   tags     = local.tags
+# }
+# # build any needed vNets
+# module "vNets" {
+#   source        = "./modules/Network/vNets"
+#   networkName   = "scaffold-core"
+#   location      = "eastus"
+#   tags          = local.tags
+#   BaseNet       = var.BaseNet
+#   SubNets       = local.Scaffold-Core-SubNets # the list of Subnets to create for the vNet
+#   resourceGroup = "gh-networking"
 
 
-  security_rules = concat(var.SQLSecRules, var.IISSecRules) #these are loose at best. We still need to setup ASG's
-  //security_rules = var.nsg-gh-scaffold-mssql-Rules
-  secGroupName = "GlobalHealthNSGSecurity" #this will eventually need to be modified to reflect the subnet/environment
-
-  depends_on = [
-    module.resourceGroup
-  ]
-}
-
-# module "Network_Building" {
-#   source         = "./modules/Network"
-#   subNet         = "1"                                      # not currently in use
 #   security_rules = concat(var.SQLSecRules, var.IISSecRules) #these are loose at best. We still need to setup ASG's
-#   secGroupName   = "GlobalHealthNSGSecurity"                #this will eventually need to be modified to reflect the subnet/environment
-#   resourceGroup  = azurerm_resource_group.gh-networking.name
-#   networkName    = "scaffold-core"
+#   //security_rules = var.nsg-gh-scaffold-mssql-Rules
+#   secGroupName = "GlobalHealthNSGSecurity" #this will eventually need to be modified to reflect the subnet/environment
+
+#   depends_on = [
+#     module.resourceGroup
+#   ]
 # }
 
+# # module "Network_Building" {
+# #   source         = "./modules/Network"
+# #   subNet         = "1"                                      # not currently in use
+# #   security_rules = concat(var.SQLSecRules, var.IISSecRules) #these are loose at best. We still need to setup ASG's
+# #   secGroupName   = "GlobalHealthNSGSecurity"                #this will eventually need to be modified to reflect the subnet/environment
+# #   resourceGroup  = azurerm_resource_group.gh-networking.name
+# #   networkName    = "scaffold-core"
+# # }
 
 
 
-# resource "azurerm_resource_group" "vonGlobalHealhRG" {
-#   name     = "vonGHresourceGroup"
-#   location = "eastus"
-# }
+
+# # resource "azurerm_resource_group" "vonGlobalHealhRG" {
+# #   name     = "vonGHresourceGroup"
+# #   location = "eastus"
+# # }
 
 
 
-# #create a sql Database :)
-# module "envSqlDatabase" {
-#   source       = "./modules/SqlServers"
-#   resoureGroup = azurerm_resource_group.vonGlobalHealhRG.name
-#   location     = azurerm_resource_group.vonGlobalHealhRG.location
-#   sqlName      = "globalhealth-sql${var.instance}" # note: We currently have this name in the existing Bonfire env. Names must be unique in azure (all of azure)
-#   sqlLogin     = "tdarttAdmin"
-#   sqlPassword  = "Squ!dD@ncer"
-#   databases    = tolist(var.dbNames)
-#   tags = {
-#     "Owner"    = "VON"
-#     "Creator"  = "Tim"
-#     "instance" = "${var.instance}"
-#   }
-# }
+# # #create a sql Database :)
+# # module "envSqlDatabase" {
+# #   source       = "./modules/SqlServers"
+# #   resoureGroup = azurerm_resource_group.vonGlobalHealhRG.name
+# #   location     = azurerm_resource_group.vonGlobalHealhRG.location
+# #   sqlName      = "globalhealth-sql${var.instance}" # note: We currently have this name in the existing Bonfire env. Names must be unique in azure (all of azure)
+# #   sqlLogin     = "tdarttAdmin"
+# #   sqlPassword  = "Squ!dD@ncer"
+# #   databases    = tolist(var.dbNames)
+# #   tags = {
+# #     "Owner"    = "VON"
+# #     "Creator"  = "Tim"
+# #     "instance" = "${var.instance}"
+# #   }
+# # }
