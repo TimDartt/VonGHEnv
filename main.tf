85e625f2d10a269eaba17a0e79b0d809cbb3d580
TimDartt <tdartt@gmail.com>
2023-03-20 14:54:07 -0400

    added source_address_prefix

--- main.tf
+++ main.tf
@@ -61,6 +61,7 @@ resource "azurerm_network_security_group" "example" {
       direction                  = security_rule.value.direction
       access                     = security_rule.value.access
       source_port_range          = security_rule.value.source_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
       destination_port_range     = security_rule.value.destination_port_range
       destination_address_prefix = security_rule.value.destination_address_prefix
     }
