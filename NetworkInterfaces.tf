08ca63c2fc6eb76101b001328a3592d3df208e0d
TimDartt <tdartt@gmail.com>
2023-03-27 11:00:46 -0400

    more nics!

--- NetworkInterfaces.tf
+++ NetworkInterfaces.tf
@@ -16,15 +16,15 @@ variable "NetworkInterface" {
     {
       nicname                       = "gh-df-private-nic"
       resource_group_name           = "gh-networking"
-      enable_accelerated_networking = FALSE
-      enable_ip_forwarding          = FALSE
+      enable_accelerated_networking = false
+      enable_ip_forwarding          = false
       ip_configuration = [
         {
 
 
           name = "privateEndpointIpConfig.2fae0a38-7e70-44f6-b4ef-570752d741eb"
           subnet_id_name : "core-routing",
-          private_Ip_address_allocation : "Dynamic",
+          private_ip_address_allocation : "Dynamic",
 
         }
       ]
@@ -32,13 +32,13 @@ variable "NetworkInterface" {
     {
       nicname                       = "gh-scaffold-api-private.nic.77c1e40a-d1ac-4979-a106-39111e98ce46"
       resource_group_name           = "gh-networking"
-      enable_accelerated_networking = FALSE
-      enable_ip_forwarding          = FALSE
+      enable_accelerated_networking = false
+      enable_ip_forwarding          = false
       ip_configuration = [
         {
           name = "privateEndpointIpConfig.17fee07e-ac77-430d-aeaf-0e4d8efd59f7"
           subnet_id_name : "gh-internal-1",
-          private_Ip_address_allocation : "Dynamic",
+          private_ip_address_allocation : "Dynamic",
         }
       ]
     },
@@ -46,13 +46,13 @@ variable "NetworkInterface" {
 
       nicname                       = "gh-scaffold-mssql-private-nic"
       resource_group_name           = "gh-networking"
-      enable_accelerated_networking = FALSE
-      enable_ip_forwarding          = FALSE
+      enable_accelerated_networking = false
+      enable_ip_forwarding          = false
       ip_configuration = [
         {
           name = "privateEndpointIpConfig.2fae0a38-7e70-44f6-b4ef-570752d741eb"
           subnet_id_name : "gh-internal-1",
-          private_Ip_address_allocation : "Dynamic",
+          private_ip_address_allocation : "Dynamic",
 
         }
       ]
@@ -60,17 +60,13 @@ variable "NetworkInterface" {
     {
       nicname                       = "gh-scaffold-sql-storage-private-nic"
       resource_group_name           = "gh-networking"
-      enable_accelerated_networking = FALSE
-      enable_ip_forwarding          = FALSE
+      enable_accelerated_networking = false
+      enable_ip_forwarding          = false
       ip_configuration = [
         {
           name = "privateEndpointIpConfig.32efcc72-5fde-4f34-87bc-7488cfdb5efe"
           subnet_id_name : "gh-internal-1",
-          private_Ip_address_allocation : "Dynamic",
-
-          name = ""
-          subnet_id_name : "",
-          private_Ip_address_allocation : "Dynamic",
+          private_ip_address_allocation : "Dynamic",
 
         }
       ]
@@ -79,17 +75,33 @@ variable "NetworkInterface" {
 
       nicname                       = "gh-fhir-private-nic"
       resource_group_name           = "gh-scaffold-fhir"
-      enable_accelerated_networking = FALSE
-      enable_ip_forwarding          = FALSE
+      enable_accelerated_networking = false
+      enable_ip_forwarding          = false
       ip_configuration = [
         {
           name = "privateEndpointIpConfig.af76fd56-1a0f-4b3b-8b38-a0f02c7638af"
           subnet_id_name : "gh-internal-1",
-          private_Ip_address_allocation : "Dynamic",
+          private_ip_address_allocation : "Dynamic",
 
         }
       ]
   }]
+
+
+  #   {
+  #   nicname                       = "gh-df-private-nic"
+  #   resource_group_name           = "gh-networking"
+  #   enable_accelerated_networking = false
+  #   enable_ip_forwarding          = false
+  #   ip_configuration = [
+  #     {
+  #       name                          = "internal"
+  #       subnet_id_name                = "gh-internal-1"
+  #       private_ip_address_allocation = "Dynamic"
+  #     }
+  #   ]
+  # }
+
 }
 
 /*
